c8521dee51c00b6230da8bc49ae192bd73b97bfb
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-04-05 18:56:27 -0300

    alteração na classe repositorio

--- perfilProfissional.py
+++ perfilProfissional.py
@@ -1,4 +1,4 @@
 import json
 from repositorio import RepositorioProfessionalProfile
-
 profissionalProfile = RepositorioProfessionalProfile()
+profissionalProfile.find("residencia", "belem")
--- repositorio.py
+++ repositorio.py
@@ -8,7 +8,20 @@ class RepositorioProfessionalProfile:
         self.load()
 
     def save(self, profiles):
-        self.profiles.append(profiles)
+        self.profiles[profiles["email"]] = profiles
+
+    def findUser(self, email):
+        return self.profiles[email]
+
+    def find(self, field, search):
+        user_search = []
+        for user in self.profiles.items():
+            print(user)
+            '''if user[field] == search:
+                user_search.append(user)'''
+
+    def find_all(self):
+        return self.profiles
 
     def load(self):
         profiles_data = {}
