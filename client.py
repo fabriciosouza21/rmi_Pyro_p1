531102653bfb967bc89ac5f10a1674ae7bca3ef3
Yann Figueiredo <yann.fabricio@hotmail.com>
2021-04-06 16:00:17 -0300

    Melhorando a  exibição de dados no cliente

--- client.py
+++ client.py
@@ -1,8 +1,19 @@
 import Pyro4
 
-teste = Pyro4.Proxy("PYRO:interface@localhost:52119")
+conection = Pyro4.Proxy("PYRO:interface@localhost:52119")
 
 field = input("Qual o campo que deseja pesquisar? ").strip()
 search = input("Informe {}: ".format(field)).strip()
 
-print(teste.dados(field, search))
+users = conection.data(field, search)
+
+print('\nListando usuários\n')
+
+for user in users:
+  print('Usuário: {} {}'.format(user['nome'], user['sobrenome']))
+  print('Email: ', user['email'])
+  print('Residência: ', user['residencia'])
+  print('Formação acadêmica: ', user['formacao academica'])
+  print('Habilidades: ', user['habilidade'])
+  print('Experiência: ', user['experiencia'])
+  print('')
